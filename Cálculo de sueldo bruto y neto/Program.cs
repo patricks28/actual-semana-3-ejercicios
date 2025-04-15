c6fd0b080aa22517016af5727cfdf503900b8573
patricks28 <mayerchunga@gmail.com>
2025-04-15 16:30:30 -0500

    Ejercicio 3
    Tercer ejercicio.

---
+++ Cálculo de sueldo bruto y neto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cálculo_de_sueldo_bruto_y_neto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada: nombre, horas trabajadas, pago por hora, descuento.
+            Console.WriteLine("Ingrese el nombre del empleado: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese las horas trabajadas: ");
+            double horas = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese el pago por hora: ");
+            double pago = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingres el porcentaje de descuento por retenciones: ");
+            double retencion = Convert.ToDouble(Console.ReadLine());
+
+            //Proceso: 
+            double sueldob = horas * pago; //Calculo de sueldo bruto
+            double desc = sueldob * (retencion/100); //Calculo de descuento por retención
+            double sueldon = sueldob - desc; // Calculo del sueldo neto
+
+            //Salida
+            Console.WriteLine("Sueldo bruto: S/."+ sueldob);
+            Console.WriteLine("Descuento por retención: S/." + desc);
+            Console.WriteLine("Sueldo neto a recibir: S/."+ sueldon);
+            Console.ReadLine();
+        }
+    }
+}
