7ffa2f4a8341b7abec9e15223ac5155b2356a780
patricks28 <mayerchunga@gmail.com>
2025-04-15 15:54:35 -0500

    Ejercicio 2
    Segundo ejercicio

---
+++ Conversión de moneda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversión_de_moneda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de dinero en dólares: ");
+            double usd = double.Parse(Console.ReadLine());
+
+            double eur = Math.Round((usd * 0.93) , 2);
+            double mxn = Math.Round((usd * 17.5), 2);
+            double pen = Math.Round((usd * 3.8) , 2);
+
+            Console.WriteLine("Cantidad convertida en Euros: " + eur);
+            Console.WriteLine("Cantidad convertida en Pesos Mexicanos: " + mxn);
+            Console.WriteLine("Cantidad convertida a Soles: " + pen);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Conversión de moneda/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Conversión de moneda")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Conversión de moneda")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("33f75bf8-744a-428b-a070-274669f8e1b7")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
